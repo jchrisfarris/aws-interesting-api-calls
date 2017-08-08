0eb029aca5d7a27212e44efb75353c0ab27a2276
Chris Farris <chris.farris@turner.com>
2017-08-07 22:02:49 -0400

    Added methods to process db, added a Policy Simulator class that uses AWS API

--- code/InterestingAwsActions.py
+++ code/InterestingAwsActions.py
@@ -1,6 +1,18 @@
 
 
 import yaml
+import json
+from pprint import pprint
+import boto3
+from botocore.exceptions import ClientError
+
+
+severity_levels = {
+	'critical' 	: 0,
+	'high'		: 1,
+	'medium'	: 3,
+	'low'		: 4
+}
 
 class Action(object):
 	"""Represents the API Call"""
@@ -8,14 +20,42 @@ class Action(object):
 		super(Action, self).__init__()
 		self.db = database
 		self.name = action_name
+		if action_name == "*":
+			raise ActionLookupError("Action is *")
 		# split the action name to get the service and api_call
-		(self.service, self.api_call) = action_name.split(':')
+		try: 
+			(self.service, self.api_call) = action_name.split(':')
+		except ValueError:
+			raise ActionLookupError("Service or API Call {} is not valid".format(action_name))
 		# Load in the rest of the stuff to the namespace
 		try: 
 			self.__dict__.update(database.action_list[self.service][self.api_call])
 		except KeyError as e:
 			raise ActionLookupError("Service or API Call {} is not valid".format(e))
 
+	def is_severe(self, severity):
+		'''returns true if this action's severity is greater than the one passed in'''
+		try:
+			if severity_levels[self.Severity] <= severity_levels[severity]:
+				return(True)
+			else:
+				return(False)
+		except KeyError:
+			return(False)
+
+	def __str__(self):
+		"""when converted to a string, convert to the Service:ApiCall format"""
+		return(self.name)
+
+	# I'm sure this is totally not the right python convention here. 
+	def __repr__(self):
+		return("<InterestingAwsActions:Action {} >".format(self.name))
+
+#
+#
+############################################
+#
+#
 class ActionDatabase(object):
 	"""Represents the Database of Interesting API Calls"""
 	def __init__(self, filename):
@@ -51,25 +91,38 @@ class ActionDatabase(object):
 		"""Returns an Action Object """
 		return(Action(self, action_name))
 
-	def by_service(self, service):
+	def get_actions_by_service(self, service):
 		"""Returns a list of Actions fitting the specific service"""
 		output = []
 		for api_call in self.action_list[service]:
 			output.append(service + ":" + api_call)
 		return(output)
 
-	def by_severity(self, severity):
+	def get_api_calls_by_service(self, service):
+		"""Returns a list of Actions fitting the specific service"""
+		output = []
+		for api_call in self.action_list[service]:
+			output.append(api_call)
+		return(output)
+
+	def get_actions_by_severity(self, severity):
 		"""Return a list of all api_calls where Severity = <severity>"""
 		return(self.__get_actions_by_key__("Severity", severity))
 
-	def by_category(self, category):
+	def get_actions_by_severity_filter(self, severity_filter):
+		output = []
+		for s in severity_levels.keys():
+			if severity_levels[s] <= severity_levels[severity_filter]:
+				output = output + self.get_actions_by_severity(s)
+		return(output)
+
+	def get_actions_by_category(self, category):
 		"""Return a list of all api_calls where Category = <category>"""
 		return(self.__get_actions_by_key__("Category", category))
 
-	def by_risk(self, risk):
+	def get_actions_by_risk(self, risk):
 		"""Return a list of all api_calls where Risk = <risk>"""
 		return(self.__get_actions_by_key__("Risk", risk))
-
 
 	def __get_actions_by_key__(self, key, value):
 		output = []
@@ -82,8 +135,8 @@ class ActionDatabase(object):
 		return(output)
 
 	def list_services(self):
+		"""Returns a list of services that appear in the database"""
 		return(self.action_list.keys())
-
 
 	def list_severities(self):
 		"""Returns a list of valid severities in the database"""
@@ -131,6 +184,94 @@ class ActionDatabase(object):
 		return(output)
 
 
+
+	def get_actions_by_iam_action_string(self, action_wildcard):
+		"""
+		Returns a list of actions that match the iam action string. 
+		ex "ec2:Create*" will return ec2:CreateRoute, ec2:CreateVPC, etc.
+		likewise ec2:*VPC should return ec2:DescribeVPC and ec2:CreateVPC, but not ec2:CreateVPCEndpoint
+		"""
+		(service, api_wildcard) = action_wildcard.split(':')
+		output = []
+
+		if '*' not in action_wildcard:
+			if api_wildcard in self.get_api_calls_by_service(service):
+				return([Action(self,action_wildcard)])
+			else:
+				return([])
+		
+		if api_wildcard == "*":
+			for a in self.get_api_calls_by_service(service):
+				output.append(Action(self, service + ":" + a))
+		else:
+			api_wildcard_substr = action_wildcard.replace("*", "")
+			for a in self.get_api_calls_by_service(service):
+				if api_wildcard_substr in a:
+					try:
+						output.append(Action(self, service + ":" + a))
+					except ActionLookupError:
+						print("Weird. {} is a substr of {}, yet the Action Lookup failed".format(api_wildcard_substr, a))
+						pass
+		# print("found actions for {} of {}".format(action_wildcard, output))
+		return(output)
+
+	def sort_actions_by_category(self, action_list):
+		'''Given a list of iam actions, return a dict of arrays where the dict key is the action's category in the db'''
+		output = {}
+		for a in action_list:
+			action = Action(self, a)
+			if action.Category in output:
+				output[action.Category].append(a)
+			else:
+				output[action.Category] = [a]
+		return(output)
+
+
+class PolicySimulator(object):
+	"""execute Policy Simulations for a User/Group/Role based on the InterestingActions Database"""
+	def __init__(self, action_file, creds):
+		super(PolicySimulator, self).__init__()
+		self.action_file = action_file
+		self.creds = creds
+		self.action_db = ActionDatabase(action_file)
+		self.iam_client = boto3.client('iam', 		
+			aws_access_key_id = creds['AccessKeyId'],
+			aws_secret_access_key = creds['SecretAccessKey'],
+			aws_session_token = creds['SessionToken']
+		)
+
+	def simulate_resource(self, resource_arn, severity_filter):
+		'''return a list of all interesting api calls resource can execute that are higher than the severity filter'''
+		output = []
+		actions_of_interest = self.action_db.get_actions_by_severity_filter(severity_filter)
+		try:
+			response = self.iam_client.simulate_principal_policy (
+				PolicySourceArn=resource_arn,
+				ActionNames=actions_of_interest
+			)
+			while response['IsTruncated'] == "True":
+				for e in response['EvaluationResults']:
+					if e['EvalDecision'] != "allowed":
+						continue
+					output.append(e.EvalActionName)
+				response = self.iam_client.simulate_principal_policy (
+					PolicySourceArn=resource_arn,
+					ActionNames=actions_of_interest,
+					MaxItems=100, Marker=response['Marker']
+				)
+
+			for e in response['EvaluationResults']:
+				if e['EvalDecision'] != "allowed":
+					continue
+				output.append(e['EvalActionName'])
+			if len(actions_of_interest) == len(output):
+				return(["Full Administrative Access"])
+			else:
+				return(output)
+		except ClientError as e:
+			print("Error Simulating Policy: {}".format(e))
+			exit(1)
+		
 
 class ActionFileParseError(Exception):
 	'''Raised when the Action yaml file has a syntax error'''
